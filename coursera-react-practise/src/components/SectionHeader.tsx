42b94db0d4ce4a2e97f7a1a6d75446d527becd55
Dimitrios Stamatakis <45168324+jimzord12@users.noreply.github.com>
2024-12-05 17:49:24 +0200

    Update SectionHeader.tsx

--- coursera-react-practise/src/components/SectionHeader.tsx
+++ coursera-react-practise/src/components/SectionHeader.tsx
@@ -5,7 +5,7 @@ interface SectionHeaderProps {
 const SectionHeader = ({ title }: SectionHeaderProps) => {
   return (
     <div className="flex relative justify-center mt-4">
-      <div className="absolute w-full h-4 bg-slate-500 top-9" />
+      <div className="absolute w-full h-4 bg-slate-500 top-7" />
       <div className="w-1/3 bg-slate-700 py-4 rounded-lg z-10">
         <h1 className="text-3xl">{title}</h1>
       </div>
